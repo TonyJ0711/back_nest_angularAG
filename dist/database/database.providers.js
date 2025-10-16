5ce18e99e04445c7f659b953cf238838f794c833
Lorecami <clg.vivas@yavirac.edu.ec>
2025-10-16 13:00:31 -0500

    nuevo comit prueba

---
+++ dist/database/database.providers.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.databaseProviders = void 0;
+const typeorm_1 = require("typeorm");
+exports.databaseProviders = [
+    {
+        provide: 'DATA_CONNECTION',
+        useFactory: async () => {
+            const dataSource = new typeorm_1.DataSource({
+                type: 'postgres',
+                host: 'localhost',
+                port: 5432,
+                username: 'postgres',
+                password: '12345',
+                database: 'backend_AG',
+            });
+            return dataSource.initialize();
+        }
+    }
+];
+//# sourceMappingURL=database.providers.js.map
