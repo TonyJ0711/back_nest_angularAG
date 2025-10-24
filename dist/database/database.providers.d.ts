cb36004469588e1c84cb8070bab5d01f93196aa3
Lorecami <clg.vivas@yavirac.edu.ec>
2025-10-24 13:30:48 -0500

    Cambios clase24 xd

--- dist/database/database.providers.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigService } from "src/config/config.service";
-import { DataSource } from "typeorm";
-export declare const databaseProviders: {
-    provide: string;
-    inject: (typeof ConfigService)[];
-    useFactory: (config: ConfigService) => Promise<DataSource>;
-}[];
--- dist/modules/auth/dto/login-auth.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { User } from "../interfaces/user.interfaces";
-export declare class LoginAuthDto implements User {
-    email: string;
-    password: string;
-}
